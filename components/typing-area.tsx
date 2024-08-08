104b5405856e0487001b8fcbb3a9360244ac21c6
Андрей Романов <human1488pwnz@yandex.ru>
2024-08-09 00:18:04 +0300

    TypingArea component

---
+++ components/typing-area.tsx
@@ -0,0 +1,23 @@
+interface TypingAreaProps {
+    typingRef: React.RefObject<HTMLDivElement>
+    onInput: (value: string) => void
+}
+
+export const TypingArea = ({ typingRef, onInput }: TypingAreaProps) => {
+    const handleInput = (e: React.FormEvent<HTMLDivElement>) => {
+        const value = e.currentTarget.innerText.replace(/\n/g, '')
+
+        onInput(value)
+    }
+
+    return (
+        <div
+            ref={typingRef}
+            contentEditable={true}
+            onInput={handleInput}
+            spellCheck={false}
+            suppressContentEditableWarning={true}
+            className="absolute w-1 h-1 overflow-hidden text-transparent caret-transparent outline-none whitespace-pre-wrap cursor-default z-10"
+        />
+    )
+}
