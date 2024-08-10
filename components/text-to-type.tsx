782cb4d3c90244bf6261126e1f7c66fe887aace1
Андрей Романов <human1488pwnz@yandex.ru>
2024-08-10 17:37:08 +0300

    Improved readability

--- components/text-to-type.tsx
+++ components/text-to-type.tsx
@@ -15,56 +15,62 @@ export const TextToType = ({ textToType, typedText, errorMap, isActive }: TextTo
     const [words, setWords] = useState<string[][]>([]);
 
     useEffect(() => {
-        // Разбиваем текст на слова и затем на буквы, включая пробелы между словами
-        const splitWords = textToType.split(' ').map(word => word.split(''));
-        setWords(splitWords);
-    }, [textToType]);
+        const splitWords = textToType.split(' ').map(word => word.split(''))
+
+        setWords(splitWords)
+    }, [textToType])
 
     return (
-        <div className="text-4xl leading-relaxed pointer-events-none">
+        <div className="flex flex-wrap gap-x-3 text-4xl tracking-wider leading-relaxed pointer-events-none">
+
             {words.map((word, wordIndex) => {
                 const wordStartIndex = words
                     .slice(0, wordIndex)
                     .reduce((acc, w) => acc + w.length + 1, 0); // +1 для учета пробела
 
                 return (
-                    <div key={wordIndex} className="inline-block mr-2">
-                        {word.map((char, charIndex) => {
-                            const globalIndex = wordStartIndex + charIndex;
-                            const typedChar = typedText[globalIndex];
+                    <div key={wordIndex} className="flex">
 
-                            let color = 'text-neutral-500/60';
+                        {word.map((char, charIndex) => {
+                            const globalIndex = wordStartIndex + charIndex
+                            const typedChar = typedText[globalIndex]
+
+                            let color = 'text-neutral-500/60'
 
                             if (typedChar) {
                                 if (typedChar === char) {
-                                    color = 'text-green-500';
+                                    color = 'text-green-500'
                                 } else if (errorMap[globalIndex]) {
-                                    color = 'text-rose-500';
+                                    color = 'text-rose-500'
                                 }
                             }
 
                             return (
                                 <span key={globalIndex} className="relative">
+
                                     {/* Отображение курсора перед текущим символом */}
                                     {globalIndex === typedText.length && (
                                         <span
                                             className={clsx(
-                                                'absolute -left-1 text-transparent',
+                                                'absolute -left-1.5 text-transparent',
                                                 isActive && 'text-yellow-400'
                                             )}
                                         >
                                             |
                                         </span>
                                     )}
+
                                     <span className={color}>
                                         {char}
                                     </span>
                                 </span>
-                            );
+                            )
                         })}
+
                     </div>
-                );
+                )
             })}
+
         </div>
     );
 };
