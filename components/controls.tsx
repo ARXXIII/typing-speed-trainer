3ac32aa7a1cb2d43dc5e3d2228a7b2a4e25f4dc0
Андрей Романов <human1488pwnz@yandex.ru>
2024-08-10 17:37:37 +0300

    Design changed

--- components/controls.tsx
+++ components/controls.tsx
@@ -15,7 +15,6 @@ export const Controls = ({ wordCount, setWordCount, onRestart }: ControlsProps) 
     return (
         <div className="flex justify-center items-center gap-x-6 p-3 font-medium rounded-lg bg-zinc-800/30">
             <h1 className="text-neutral-500/60">words</h1>
-            <div className="hidden lg:block w-12 h-0.5 rounded-full bg-neutral-500/60" />
             <button onClick={() => handleWordCountChange(10)} className={clsx('text-neutral-500/60 hover:text-neutral-400 transition-colors',
                 wordCount === 10 && 'text-yellow-400 hover:text-yellow-400'
             )}>
